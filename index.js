202c38973a25a09b7e31f37417162987239341b4
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-12-29 19:47:24 +0600

    Basic server setup initiated

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Taskboarders server is running!");
+});
+
+app.listen(port, () =>
+  console.log(`Taskboarders server is listening to port ${port}`)
+);
